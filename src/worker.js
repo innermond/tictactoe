0978611104e76cfa20e772bfd0825a6c28e6e7cd
innermond <innermond@yahoo.com>
2026-01-14 22:46:29 +0200

    refactor: simplification

--- src/worker.js
+++ src/worker.js
@@ -2,40 +2,47 @@ import { WORK_TERMINATE, WORK_CHECK_WINNER } from "./work-constants.js";
 
 let doTerminateAll = false;
 let activeRuns = [];
+let currentRunId = null;
 
-const giveControlToEventLoop = () => new Promise(r => setTimeout(r, 500));
 
 const checkWinner = async (payload, id) => {
   console.log(`call id ${id}`)
   doTerminateAll = false;
   activeRuns.push(id);
+  currentRunId = id;
+
 
   let startTime = performance.now();
-  const val = await new Promise((resolve) => setTimeout(async () => {
-    // CPU bound here
-    for (let i = 0; i < 300_000_000; i++) {
-      if (i % 100_000_000 === 0) {
-        await giveControlToEventLoop();
-        if (doTerminateAll) {
-          console.log("terminate all")
-          resolve([2, 4]);
-          return;
-        }
+  let result = null;
+  // CPU bound here
+  for (let i = 0; i < 300_000_000; i++) {
+    if (i % 100_000_000 === 0) {
+      await new Promise(r => setTimeout(r, 500));
+      if (doTerminateAll) {
+        console.log("terminate all")
+        result = [2, 4];
+        break;
+      }
+      if (currentRunId !== id) {
+        console.log(`terminate ${id} with ${payload} from ${currentRunId}`)
+        result = [2, 4];
         const index = activeRuns.indexOf(id);
-        if ((activeRuns.length > 1 && index < activeRuns.length - 1)) {
-          console.log(`terminate ${id} with ${payload} from ${activeRuns[activeRuns.length - 1]}`)
-          resolve([2, 4]);
-          activeRuns.splice(index, 1);
-          return;
-        }
+        if (index !== -1) activeRuns.splice(index, 1);
+        break;
       }
     }
-    resolve([4, 2]);
-  }, 1000));
+  }
+
+  if (result === null) {
+    result = [4, 2];
+    const index = activeRuns.indexOf(id);
+    if (index !== -1) activeRuns.splice(index, 1);
+  }
+
   let endTime = performance.now()
   const elapsed = endTime - startTime;
-  console.log(`${val} after 1s? elapsed time is ${elapsed / 1_000}??`)
-  self.postMessage(val);
+  console.log(`${result} after 1s? elapsed time is ${elapsed / 1_000}??`)
+  self.postMessage(result);
 };
 
 onmessage = async (e) => {
